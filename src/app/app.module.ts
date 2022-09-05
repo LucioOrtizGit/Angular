78a101a9c237e8476fd3f65235e0abc4fb51f1a7
LucioOrtizGit <lukefilms@gmail.com>
2022-09-05 19:11:15 +0200

    DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContadorModule } from './contador/contador.module';
 
 
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+
+@Component({
+  selector: 'app-dragon-ball',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  
+  nuevo:Personaje={
+    nombre:'Roshi',
+    poder:1000
+  }
+
+  constructor(){}
+
+  
+
+}
