e8df8c2d7a04a1ea968fa19a3ea02fd61dfc1115
LucioOrtizGit <lukefilms@gmail.com>
2022-09-05 16:12:10 +0200

    cambio

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -18,6 +18,7 @@ export class ContadorComponent {
     titulo: string = 'Contador App';
     numero: number = 10;
     base:number = 5;
+    
   
     acumular(valor : number){
       this.numero += valor;
